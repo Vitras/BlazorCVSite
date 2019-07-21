574db87c988ced1a39f82b0254beee495737e264
Kai <azureflarekai@gmail.com>
2019-07-21 19:21:39 +0200

    Initial push, setup

---
+++ AppState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorClientSample
+{
+    public static class AppState
+    {
+        public static bool HideSideBar
+        {
+            get
+            {
+                return SideBarItems == null || SideBarItems.Count == 0;
+            }
+        }
+        public static List<string> SideBarItems = new List<string>();
+    }
+}
